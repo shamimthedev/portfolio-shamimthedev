667a1d7b2a3ab7f1fea4d956bd8f486ea41ee2d6
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-05 02:21:27 +0600

    (feat) added and updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,13 @@
-
-
 const About = () => {
   return (
-    <div id="about">
-
-      <h1>About Page</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, atque rerum quas eveniet vel sit veniam, accusantium eaque optio neque explicabo voluptatem. Modi quibusdam aspernatur in, eos molestias corrupti quis tenetur numquam est praesentium omnis ipsum id architecto nulla quae soluta excepturi error ab, alias vero iure inventore, debitis non. Quaerat nobis est inventore vero, quas nesciunt asperiores eligendi nostrum magnam temporibus laudantium esse facere itaque illo ducimus dolorum fuga velit! Dolorem impedit corporis perspiciatis perferendis tempora vel officiis ratione itaque quaerat, exercitationem molestias, hic praesentium temporibus dolor! Nisi blanditiis rerum quos consectetur, cumque ut, molestiae quaerat ab voluptas porro praesentium quod reiciendis accusantium sit voluptate sed. Quasi, porro cum. Illo error exercitationem deserunt ea eaque vel, quae, debitis libero corrupti odio ab veritatis necessitatibus corporis neque repellat accusamus perferendis tenetur non iure molestiae earum fuga voluptate. Porro reprehenderit qui temporibus obcaecati ullam minima quod quis perferendis eveniet, commodi ipsum officiis dolor doloribus fugiat assumenda? Amet quasi facilis molestias voluptas inventore sit optio voluptatibus molestiae commodi deleniti! Consequuntur tempore amet, maxime maiores nostrum at culpa eos possimus fugit doloremque quaerat odio atque quae quod laborum dignissimos hic? Iure beatae officiis delectus impedit laudantium, sed laborum eaque hic modi? Enim, amet!</p>
-    </div>
+    <section id="about" className="w-full bg-[#F4F4F4] py-[69px]">
+      <div className="max-w-[1320px] mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+        <h2 className="font-semibold text-[30px] text-center lg:text-left">About me</h2>
+        <p className="max-w-[900px] mt-[15px] text-[15px] text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+      </div>
+    </section>
   )
 }
 
--- src/components/Specialization.jsx
+++ src/components/Specialization.jsx
@@ -1,8 +1,37 @@
-
+import JSIcon from '/js-icon.svg'
+import BootstrapIcon from '/bootstrap-icon.svg'
+import Html5Icon from '/html5-icon.svg'
+import CSS3Icon from '/css3-icon.svg'
+import GitIcon from '/git-icon.svg'
+import TailwindIcon from '/tailwind-css.svg'
 
 const Specialization = () => {
   return (
-    <div>Specialization</div>
+    <section id="specialization" className="max-w-[1320px] mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <h2 className="font-semibold text-[30px] text-center lg:text-left">Specializations</h2>
+      <p className="max-w-[900px] mt-[15px] text-[15px] text-justify">Technologies I'm familiar with </p>
+
+      <div className="web-icons flex justify-between flex-wrap">
+        <div className="JS h-[150px] w-[150px] rounded-[7px] overflow-hidden">
+        <img src={JSIcon} alt="JSIcon" />
+        </div>
+        <div className="Html5 h-[150px] w-[150px] rounded-[7px] bg-[#c4c4c4] flex items-center justify-center">
+          <img src={Html5Icon} alt="Html5Icon" />
+        </div>
+        <div className="CSS3 h-[150px] w-[150px] rounded-[7px] bg-[#828282] flex items-center justify-center">
+          <img src={CSS3Icon} alt="CSS3Icon" />
+        </div>
+        <div className="Git h-[150px] w-[150px] rounded-[7px] bg-[#9E9E9E] flex items-center justify-center">
+          <img src={GitIcon} alt="GitIcon" />
+        </div>
+        <div className="bootstrap h-[150px] w-[150px] rounded-[7px] bg-[#c4c4c4] flex items-center justify-center">
+          <img src={BootstrapIcon} alt="BootstrapIcon" />
+        </div>
+        <div className="tailwind h-[150px] w-[150px] rounded-[7px] bg-[#c4c4c4] flex items-center justify-center">
+          <img src={TailwindIcon} alt="TailwindIcon" />
+        </div>
+      </div>
+    </section >
   )
 }
 
